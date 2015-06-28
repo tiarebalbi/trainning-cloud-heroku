b75055da2018e1ecb42a5371c48eda29989c9023
Tiare Balbi Bonamini <me@tiarebalbi.com>
2015-06-28 15:07:15 -0300

    Publicando aplicação no heroku

--- src/main/java/com/tiarebalbi/trainning/spring/SampleController.java
+++ src/main/java/com/tiarebalbi/trainning/spring/SampleController.java
@@ -1,7 +1,7 @@
 package com.tiarebalbi.trainning.spring;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller Sample
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author Tiarê Balbi Bonamini
  * @version 1.0.0
  */
-@Controller
+@RestController
 public class SampleController {
 
     @RequestMapping("/")
